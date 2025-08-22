7d68493b95e6fa555272129508f3a6bf7e06618c
Oshin Lilani <oshinlilani08@gmail.com>
2025-08-22 20:08:50 +0530

    Initial commit - NotesApp

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors'
+import connectToMongoDB from './db/db.js';
+import dotenv from "dotenv";
+
+import authRouter from './routes/auth.js'
+import noteRouter from './routes/note.js'
+
+dotenv.config();
+
+const  app = express() 
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth', authRouter)
+app.use('/api/note', noteRouter)
+
+
+app.listen(5000, () => {
+    connectToMongoDB()
+    console.log("server is running");
+})
