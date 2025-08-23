5d1ee3ce44b1585df94eb8ce9eb38f9f73fa131a
Oshinlilani <139527532+Oshinlilani@users.noreply.github.com>
2025-08-23 10:55:23 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://oshinlilani40:CFBN6hidAccVLbln@cluster0.goerhye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("Error connecting to MongoDB", error.message);
